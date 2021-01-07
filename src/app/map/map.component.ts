a3d5b56cca738667c43039f4d730f3281df9492a
gustavokessler <gustavo.kessler@hotmail.com>
2021-01-07 12:35:06 -0300

    avanços dos mapas

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { circle, latLng, polygon, tileLayer } from 'leaflet';
-
+import { circle, latLng, marker, polygon, tileLayer } from 'leaflet';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -14,18 +13,38 @@ export class MapComponent implements OnInit {
       tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
     ],
     zoom: 5,
-    center: latLng(0, 0)
+    center: latLng(-29.49944444, -52.51527778)
   };
 
-  layersControl = {
-    baseLayers: {
-      'Open Street Map': tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
-      'Open Cycle Map': tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-    },
-    overlays: {
-      'Big Circle': circle([ 46.95, -122 ], { radius: 5000 }),
-      'Big Square': polygon([[ 46.8, -121.55 ], [ 46.9, -121.55 ], [ 46.9, -121.7 ], [ 46.8, -121.7 ]])
-    }
+  teste = "bacia1"
+
+  layers = [
+    circle([-29.49944444, -52.51527778], {
+      radius: 5000,
+      color: "blue",
+      fillOpacity: 0.1
+    }).bindPopup(
+      '<label><b>Nome:</b> ' + 'Barros Cassal</label><br>' +
+      '<b><label> Última coleta: </b>' + '2009' + '<br>' +
+      '<label><b>Alcalinidade de bicarbonatos:</b> ' + '22,40' + ' (CaCO3) ' + '</label><br>' +
+      '<label><b>Alcalinidade de bicarbonatos:</b> ' + '<0,1' + ' (CaCO3) ' + '</label>' +
+      '<label><b>Alcalinidade de bicarbonatos:</b> ' + '<0,1' + ' mg/L ' + '</label>' 
+    ),
+    // polygon([[ 46.8, -121.85 ], [ 46.92, -121.92 ], [ 46.87, -121.8 ]]),
+    // marker([ -29.49944444, -52.51527778 ], {
+
+    // })
+  ];
+
+  pointMouseover(leafletEvent: any): any {
+    console.log(leafletEvent);
+    var layer = leafletEvent.target;
+
+    layer.setStyle({
+      weight: 2,
+      color: '#666',
+      fillColor: 'white'
+    });
   }
 
   constructor() { }
