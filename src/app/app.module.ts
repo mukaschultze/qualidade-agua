2a3622179a7ca19f9d46f1031ffd1c6adc9bddcb
Muka Schultze <samuelschultze@gmail.com>
2021-01-26 11:36:25 -0300

    github button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
@@ -33,6 +34,7 @@ import { SearchBarComponent } from './nav-bar/nav-bar.component';
     MatFormFieldModule,
     MatInputModule,
     MatProgressBarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
